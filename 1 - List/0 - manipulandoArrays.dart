cc3362c9c9e2d44389511b637ce2a3f5678e83fb
Gabriel Christo <gabrielchristo@hotmail.com>
2021-03-11 22:33:59 -0300

    Listas

---
+++ 1 - List/0 - manipulandoArrays.dart
@@ -0,0 +1,54 @@
+main(List<String> args) {
+  var numeros = List.generate(10, (index) => index);
+
+  // for(var i = 0; i < numeros.length; i++){
+  //   print(numeros[i]);
+  // }
+
+  // for (var numero in numeros) {
+  //   print(numero);
+  // }
+
+  // !forma de fazer um for em uma lista usando forEach
+  // numeros.forEach((numero) => print(numero));
+
+  // !Expand, usado para concatenar lista
+  // var lista = [[1, 2], [3, 4]];
+  // var novaLista = lista.expand((lista) => lista);
+  // print(lista);
+  // print(novaLista);
+  // novaLista.forEach((l) => print(l));
+
+  // !map
+  // List<int> lista = [1, 2, 3];
+  // List<String> novaListaString = lista.map((e) => '$e - Gabriel').toList();
+  // print(lista);
+  // print(novaListaString);
+
+  // !any
+  // List<String> listaBusca = ['Felipe', 'Thalita', 'Gabriel'];
+  // if(listaBusca.any((nome) => nome == 'Gabriel')){
+  //   print('Tem Gabriel');
+  // }else{
+  //   print('Não tem Gabriel');
+  //}
+
+  // !every
+  List<String> listaBusca = ['Felipe', 'Thalita', 'Gabriel'];
+  if(listaBusca.every((nome) => nome.contains('a'))) {
+    print('Todos tem a letra a');
+  }else{
+    print('Nem todos tem a letra a');
+  }
+
+  // !where
+  var nome = listaBusca.where((element) => element == 'Gabriel');
+  print(nome);
+
+  // !fold
+  var numerosParaSoma = [2, 2, 2];
+  var somandoValores = numerosParaSoma.fold(0, (atual, elemento) => atual+elemento);
+  print(somandoValores);
+
+
+}
