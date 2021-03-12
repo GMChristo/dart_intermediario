e17ca0cb7cc92c20e18f030b8513d0c687666ff6
Gabriel Christo <gabrielchristo@hotmail.com>
2021-03-12 10:54:53 -0300

    Listas - Sort

--- 1 - List/0 - manipulandoArrays.dart
+++ 1 - List/0 - manipulandoArrays.dart
@@ -1,5 +1,5 @@
 main(List<String> args) {
-  var numeros = List.generate(10, (index) => index);
+  // var numeros = List.generate(10, (index) => index);
 
   // for(var i = 0; i < numeros.length; i++){
   //   print(numeros[i]);
@@ -34,21 +34,33 @@ main(List<String> args) {
   //}
 
   // !every
-  List<String> listaBusca = ['Felipe', 'Thalita', 'Gabriel'];
-  if(listaBusca.every((nome) => nome.contains('a'))) {
-    print('Todos tem a letra a');
-  }else{
-    print('Nem todos tem a letra a');
-  }
+  // List<String> listaBusca = ['Felipe', 'Thalita', 'Gabriel'];
+  // if(listaBusca.every((nome) => nome.contains('a'))) {
+  //   print('Todos tem a letra a');
+  // }else{
+  //   print('Nem todos tem a letra a');
+  // }
 
   // !where
-  var nome = listaBusca.where((element) => element == 'Gabriel');
-  print(nome);
+  // var nome = listaBusca.where((element) => element == 'Gabriel');
+  // print(nome);
 
   // !fold
-  var numerosParaSoma = [2, 2, 2];
-  var somandoValores = numerosParaSoma.fold(0, (atual, elemento) => atual+elemento);
-  print(somandoValores);
+  // var numerosParaSoma = [2, 2, 2];
+  // var somandoValores = numerosParaSoma.fold(0, (atual, elemento) => atual+elemento);
+  // print(somandoValores);
+
+  // !sort
+  var listaParaOrdenacao = [99, 22, 333, 765, 1000, 1, 10, 3, 4, 5, 6, 7, 0, 1, 2];
+  print(listaParaOrdenacao);
+
+  print('Lista Crescente');
+  listaParaOrdenacao.sort((n1, n2) => n1.compareTo(n2));
+  print(listaParaOrdenacao);
+  
+  print('Lista Decrescente');
+  listaParaOrdenacao.sort((n1, n2) => n2.compareTo(n1));
+  print(listaParaOrdenacao);
 
 
 }
