fde6ecf6544815730b78b43d27ac563cedb7912a
Gabriel Christo <gabrielchristo@hotmail.com>
2021-03-21 14:13:13 -0300

    correção desafio item3

--- desafio.dart
+++ desafio.dart
@@ -23,16 +23,16 @@ main(List<String> args) {
   var feminino = 0;
 
   pessoas.forEach((pessoa) => pessoasAtualizada.add(pessoa.split('|')));
-  
+
   // 1 - Remover os duplicados
   Set<String> listaSemDuplicata = pessoasAtualizada.map((p) => '$p').toSet();
   print(listaSemDuplicata);
-  
+
   // 2 - Me mostre a quantidade de pessoas do sexo Masculino e Feminino
   for (var pessoa in listaSemDuplicata) {
-    if (pessoa.contains("Masculino")){
+    if (pessoa.contains("Masculino")) {
       masculino++;
-    }else{
+    } else {
       feminino++;
     }
   }
@@ -41,16 +41,23 @@ main(List<String> args) {
   // 3 - Filtrar e deixar a lista somente com pessoas maiores de 18 anos
   //     e mostre a quantidade de pessoas com mais de 18 anos tambem separado por sexo
   List lista = [];
+  masculino = 0;
+  feminino = 0;
   listaSemDuplicata.forEach((p) => lista.add(p.split(',')));
   for (var p in lista) {
-    if(int.parse(p[1]) >= 18){
+    if (int.parse(p[1]) >= 18) {
+      if(p[2].toString().contains('Masculino')){
+        masculino++;
+      }else{
+        feminino++;
+      }
       maiorDeIdade.add(p);
     }
   }
+  print('Total de Homens: $masculino\nTotal de Mulheres: $feminino');
   print(maiorDeIdade);
 
   // 4 - Encontre a pessoa mais velha.
   maiorDeIdade.sort((p1, p2) => p2[1].compareTo(p1[1]));
   print(maiorDeIdade[0]);
-
 }
